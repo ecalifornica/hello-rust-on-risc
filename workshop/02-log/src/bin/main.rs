2df5e2c4e9e2d217d3fe0e08ea2aedc06d2e7e77
Robert Queenin <2177841+ecalifornica@users.noreply.github.com>
2025-11-29 15:33:45 -0500

    Switch from esp-flash to probe-rs
    Generated with:
    	esp-generate \
    	  --chip esp32c6 \
    	  -o probe-rs \
    	  --headless \
    	  02-log

--- workshop/02-log/src/bin/main.rs
+++ workshop/02-log/src/bin/main.rs
@@ -12,6 +12,7 @@ use esp_hal::{
     time::{Duration, Instant},
 };
 
+use rtt_target::rprintln;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -27,6 +28,7 @@ esp_bootloader_esp_idf::esp_app_desc!();
 fn main() -> ! {
     // generator version: 1.0.1
 
+    rtt_target::rtt_init_print!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
@@ -34,6 +36,7 @@ fn main() -> ! {
 
 
     loop {
+        rprintln!("Hello world!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
