3e7d004c4a84ba175853afa4d266cea919ff256b
Robert Queenin <2177841+ecalifornica@users.noreply.github.com>
2025-11-30 09:24:38 -0500

    workshop(02-log): migrate from standard RTT to defmt logging
    Replaces standard text-based RTT logging with deferred formatting for
    lower runtime overhead.
    
    Generated with:
    	esp-generate \
    	  --chip esp32c6 \
    	  -o probe-rs \
    	  -o defmt \
    	  --headless \
    	  02-log

--- workshop/02-log/src/bin/main.rs
+++ workshop/02-log/src/bin/main.rs
@@ -12,7 +12,7 @@ use esp_hal::{
     time::{Duration, Instant},
 };
 
-use rtt_target::rprintln;
+use defmt::info;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -28,7 +28,7 @@ esp_bootloader_esp_idf::esp_app_desc!();
 fn main() -> ! {
     // generator version: 1.0.1
 
-    rtt_target::rtt_init_print!();
+    rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let _peripherals = esp_hal::init(config);
@@ -36,7 +36,7 @@ fn main() -> ! {
 
 
     loop {
-        rprintln!("Hello world!");
+        info!("Hello world!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
